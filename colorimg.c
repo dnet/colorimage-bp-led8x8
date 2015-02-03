49dfe324dbe22505c7c8047561de64b845b9466b
András Veres-Szentkirályi <vsza@vsza.hu>
2015-02-03 08:14:31 +0100

    fixed prefix increment

--- colorimg.c
+++ colorimg.c
@@ -24,7 +24,7 @@ uint8_t cur_row = 0;
 
 ISR( TIMER1_COMPA_vect ) {
 	uint8_t bit, fbrow;
-	if (cur_row++ >= NUM_ROWS) cur_row = 0;
+	if (++cur_row >= NUM_ROWS) cur_row = 0;
 	fbrow = framebuf[cur_row];
 	for (bit = 0x80; bit; bit >>= 1) {
 		COL_PORT = (fbrow & bit) == bit ? COL_DATA : 0;
